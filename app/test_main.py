1b500ca52b495194bc42a19b02f79abc0ca62b4c
Artur Bertin <arturbertin2006@gmail.com>
2026-01-22 14:03:00 -0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,7 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "ca, da, e",
+    "cat, dog, expected",
     [
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -17,29 +17,31 @@ from app.main import get_human_age
 )
 class TestAnimal:
 
-    def test_cat_and_dog_less_than_fifteen_years(self, ca, da, e) -> None:
-        assert get_human_age(ca, da) == e
+    def test_cat_and_dog_less_than_fifteen_years(self, cat: int,
+                                                 dog: int,
+                                                 expected: list) -> None:
+        assert get_human_age(cat, dog) == expected
 
+    def test_cat_and_dog_with_fifteen_years(self, cat: int,
+                                            dog: int, expected: list) -> None:
+        assert get_human_age(cat, dog) == expected
 
-    def test_cat_and_dog_with_fifteen_years(self, ca, da, e) -> None:
-        assert get_human_age(ca, da) == e
+    def test_all_animals_with_23_years(self, cat: int,
+                                       dog: int, expected: list) -> None:
+        assert get_human_age(cat, dog) == expected
 
+    def test_all_animals_with_24_years(self, cat: int,
+                                       dog: int, expected: list) -> None:
+        assert get_human_age(cat, dog) == expected
 
-    def test_all_animals_with_23_years(self, ca, da, e) -> None:
-        assert get_human_age(ca, da) == e
+    def test_all_animals_with_27_years(self, cat: int,
+                                       dog: int, expected: list) -> None:
+        assert get_human_age(cat, dog) == expected
 
+    def test_all_animals_with_28_years(self, cat: int,
+                                       dog: int, expected: list) -> None:
+        assert get_human_age(cat, dog) == expected
 
-    def test_all_animals_with_24_years(self, ca, da, e) -> None:
-        assert get_human_age(ca, da) == e
-
-
-    def test_all_animals_with_27_years(self, ca, da, e) -> None:
-        assert get_human_age(ca, da) == e
-
-
-    def test_all_animals_with_28_years(self, ca, da, e) -> None:
-        assert get_human_age(ca, da) == e
-
-
-    def test_all_animals_with_100_years(self, ca, da, e) -> None:
-        assert get_human_age(ca, da) == e
+    def test_all_animals_with_100_years(self, cat: int,
+                                        dog: int, expected: list) -> None:
+        assert get_human_age(cat, dog) == expected
